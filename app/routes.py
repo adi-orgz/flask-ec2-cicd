e779d6e252304fdad4d116ec32588fa5778bee2b
AdityaGadekar <aadityagadekar123@gmail.com>
2025-05-08 23:39:38 +0530

    flask-ec2-cicd

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
 from flask import render_template
 from app import app
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/api/hello')
 def hello():
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,9 +1,11 @@
 from app import app
 
+
 def test_index():
     tester = app.test_client()
     response = tester.get('/')
     assert response.status_code == 200
+
 
 def test_hello():
     tester = app.test_client()
