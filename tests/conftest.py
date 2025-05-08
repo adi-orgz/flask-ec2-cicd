46086cf04d4abf701a31dd7f43f8ef9e5f7f83ef
AdityaGadekar <aadityagadekar123@gmail.com>
2025-05-09 00:24:16 +0530

    flask-ec2-cicd

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,21 @@
 import pytest
 from app import app as flask_app
 
-@pytest.fixture
-def app():
-    flask_app.config['TESTING'] = True
-    return flask_app
+@pytest.fixture(autouse=True)
+def _setup_test_reporting(request):
+    """Setup for test reporting"""
+    request.node.test_result = "❌"  # Default to failed
 
-@pytest.fixture
-def client(app):
-    return app.test_client()
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    """Update test results for reporting"""
+    outcome = yield
+    result = outcome.get_result()
+
+    if result.when == "call":
+        if result.passed:
+            item.test_result = "✅"
+        elif result.failed:
+            item.test_result = "❌"
+        elif result.skipped:
+            item.test_result = "⚠️"
